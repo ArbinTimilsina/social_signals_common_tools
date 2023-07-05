3bc5edb9ff87e2af643a03ab69d79f37db25d453
Arbin Timilsina <arbin.timilsina@gmail.com>
2023-07-05 13:30:46 -0400

    Add sumy summary

---
+++ sumy_summary.py
@@ -0,0 +1,24 @@
+from sumy.nlp.stemmers import Stemmer
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.summarizers.luhn import LuhnSummarizer as Summarizer
+from sumy.utils import get_stop_words
+from ftfy import fix_text
+
+LANGUAGE = "english"
+SUMY_TOKENIZER = Tokenizer(LANGUAGE)
+SUMY_SUMMARIZER = Summarizer(Stemmer(LANGUAGE))
+SUMY_SUMMARIZER.stop_words = get_stop_words(LANGUAGE)
+SENTENCES_COUNT = 3
+
+
+def get_sumy_summary(comments, sentences_count=SENTENCES_COUNT):
+    comments = [fix_text(comment) for comment in comments]
+    text = " ".join(comments)
+    parser = PlaintextParser.from_string(string=text, tokenizer=SUMY_TOKENIZER)
+
+    summary = SUMY_SUMMARIZER(parser.document, sentences_count=sentences_count)
+    summary = [str(sentence) for sentence in summary]
+    if not summary:
+        summary = ["None"]
+    return " ".join(summary)
